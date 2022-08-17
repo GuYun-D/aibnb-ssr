c5c2186ccdff6684dbdeaf17c45152184c6e3751
solitary-cloud <1946306647@qq.com>
2022-08-17 14:05:20 +0800

    ✨ feat(db): 封装登陆相关的mock接口

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,18 @@ import ElementPlus, { ElMessage } from 'element-plus'
 import 'element-plus/dist/index.css'
 import i18n from './language/i18n'
 
+import aribnbb from './db'
+
+router.beforeEach((to, from, next) => {
+  aribnbb.airbnbDB.openStore({
+    ...aribnbb.languageObjectStore,
+    ...aribnbb.userObjectStore
+  }).then((res: any) => {
+    console.log("初始化所有对象仓库");
+    next()
+  })
+})
+
 const app = createApp(App)
 app.config.globalProperties.$message = ElMessage
 app.use(router)
--- src/utils/indexedDB.ts
+++ src/utils/indexedDB.ts
@@ -12,32 +12,38 @@ export default class DB {
    * @param keyPath 主键名称
    * @param indexs 对象仓库字段名称
    */
-  public openStore(storeName: string, keyPath: string, indexs?: Array<string>) {
-    const request = window.indexedDB.open(this.dbName, 1)
-
+  public openStore(stores: any) {
+    const request = window.indexedDB.open(this.dbName, 4)
     return new Promise((resolve, reject) => {
       request.onsuccess = (event: any) => {
-        console.log("数据库打开成功");
-        // console.log(event);
+        console.log('数据库打开成功')
         this.db = event.target.result
+        console.log(event)
         resolve(true)
       }
       request.onerror = (event) => {
-        console.log("数据库打开失败");
-        console.log(event);
+        console.log('数据库打开失败')
+        console.log(event)
+        reject(event)
       }
       request.onupgradeneeded = (event) => {
-        console.log("数据库升级成功");
+        console.log('数据库升级成功')
         const { result }: any = event.target
-        const store = result.createObjectStore(storeName, { autoIncrement: true, keyPath })
-        if (indexs && indexs.length > 0) {
-          indexs.map((v: string) => {
-            store.createIndex(v, v, { unique: false })
-          })
-        }
-        console.log(event);
-        store.transaction.oncomplete = (event: any) => {
-          console.log("创建对象仓库成功");
+        for (const storeName in stores) { // 初始化多个ojectStore对象仓库
+          const { keyPath, indexs } = stores[storeName]
+          if (!result.objectStoreNames.contains(storeName)) { // 没有表则新建表
+            // keyPath：主键，主键（key）是默认建立索引的属性； autoIncrement：是否自增；createObjectStore会返回一个对象仓库objectStore(即新建一个表)
+            const store = result.createObjectStore(storeName, { autoIncrement: true, keyPath })
+            if (indexs && indexs.length) {
+              indexs.map((v: string) =>
+                // createIndex可以新建索引，unique字段是否唯一
+                store.createIndex(v, v, { unique: false })
+              )
+            }
+            store.transaction.oncomplete = (e: any) => {
+              console.log('创建对象仓库成功')
+            }
+          }
         }
       }
     })
@@ -52,7 +58,7 @@ export default class DB {
   updateItem(storeName: string, data: any) {
     // transition 事务，数据库的增删改查都需要通过事务来操作
     console.log(this.db.transaction);
-    
+
     const store = this.db.transaction([storeName], 'readwrite').objectStore(storeName)
     const request = store.put({ ...data, updateTime: new Date().getTime() })
     return new Promise((resolve, reject) => {
